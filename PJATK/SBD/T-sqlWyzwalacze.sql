f372753db58bebdbb6b9d18c9263012afac00afe
Mikołaj Kowalski <153086381+Mikonos211@users.noreply.github.com>
2025-04-13 20:37:03 +0200

    Update T-sqlWyzwalacze.sql

--- PJATK/SBD/T-sqlWyzwalacze.sql
+++ PJATK/SBD/T-sqlWyzwalacze.sql
@@ -18,3 +18,33 @@ RAISERROR('Nie mo¿na usuwaæ rekordów z tabeli T_listaproduktow',16,1)
 
 DELETE FROM T_ListaProduktow
 WHERE zakup = 55 AND produkt = 5;
+
+/*
+	Zadanie 2:
+Przekształć wyzwalacz z zadania 1 tak, aby dodatkowo wypisywał informację: „Usuwanie
+rekordu dla zakupu= {IdZakupu} i produktu= {IdProduktu} nie powiodło się”. Wykorzystaj w
+tym celu tabelę Deleted
+
+*/
+
+alter Trigger UzycieTabeliDeleted 
+on T_listaProduktow 
+instead of delete 
+as
+begin
+Declare @idZakupu int, @idProduktu int 
+select @idZakupu = Zakup, @idProduktu = Produkt
+from deleted
+ RAISERROR(
+        'Usuwanie rekordu dla zakupu = %d i produktu = %d nie powiodło się.',
+        16, 1,
+        @idZakupu, @idProduktu
+    );
+end
+
+select *
+from T_ListaProduktow
+WHERE zakup = 55 AND produkt = 5;
+
+DELETE FROM T_ListaProduktow
+WHERE zakup = 55 AND produkt = 5;
